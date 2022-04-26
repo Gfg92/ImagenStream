5b784a3b14567fb8317b5ebf89879c8d52f621ce
guille <gfg_11@hotmail.com>
2022-04-26 10:20:33 +0200

    Imagen

--- src/TransformaImagen.java
+++ src/TransformaImagen.java
@@ -17,8 +17,8 @@ public class TransformaImagen {
     public void transformaNegativo() throws IOException {
         // Transformar a negativo y guardar como *_n.bmp
         File file = new File("resource/" + getNombreSinExtension() + "_n.bmp");
-        InputStream input = new FileInputStream(f);
-        OutputStream output = new FileOutputStream(file);
+        InputStream input = new FileInputStream(f); // Origen
+        OutputStream output = new FileOutputStream(file); // Destino
 
         // Lee los primeros 54 bytes y los copia al fichero de salida
         byte[] metadata = input.readNBytes(54);
@@ -26,7 +26,7 @@ public class TransformaImagen {
 
         // Recorrer byte a byte e invertir
         int b = input.read();
-        while (b != -1){
+        while (b != -1) {
             int invertido = 255 - b;
             output.write(invertido);
             b = input.read();
@@ -36,16 +36,28 @@ public class TransformaImagen {
     }
 
     public void transformaOscuro() throws IOException {
-
         // Transformar a una imagen más oscura y guardar como *_o.bmp
+        File file = new File("resource/" + getNombreSinExtension() + "_o.bmp");
+        InputStream input = new FileInputStream(f); //Origen
+        OutputStream output = new FileOutputStream(file); //Destino
 
+        byte[] primerosBytes = input.readNBytes(54);
+        output.write(primerosBytes); // Escribo los primeros 54 bytes
 
+        int b = input.read();
+        while (b != -1) {
+            int oscuro = b / 2;
+            output.write(oscuro);
+            b = input.read();
+        }
+        input.close();
+        output.close();
     }
 
 
     public void transformaBlancoNegro() throws IOException {
-
         // Transformar a una imagen en blanco y negro y guardar como *_bn.bmp
+
 
     }
 
@@ -53,8 +65,8 @@ public class TransformaImagen {
 
         //Devuelve el nombre del archivo f sin extensión
         String[] fields = f.getName().split("\\.");
-
-        return fields[0]; //devuelvo la parte del nombre dejando fuera la extension
+        String nombre = fields[0];//devuelvo la parte del nombre dejando fuera la extension
+        return nombre;
 
     }
 
