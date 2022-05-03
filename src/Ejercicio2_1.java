cde623ff63f2140a06bf51ee1cce32b16be26514
guille <gfg_11@hotmail.com>
2022-05-03 12:01:19 +0200

    Imagen

--- src/Ejercicio2_1.java
+++ src/Ejercicio2_1.java
@@ -1,22 +1,19 @@
 import java.io.File;
-
 import java.io.IOException;
 
 public class Ejercicio2_1 {
 
     public static void main(String[] args) throws IOException {
 
-        
 
         File f = new File("./resource/penyagolosa.bmp");
 
-        
 
         TransformaImagen ti = new TransformaImagen(f);
 
         ti.transformaNegativo();
-ti.transformaEscalaGrises();
-ti.transformaBlancoNegro(200);
+        ti.transformaEscalaGrises();
+        ti.transformaBlancoNegro(200);
         ti.transformaOscuro();
 //
 //        ti.transformaBlancoNegro();
